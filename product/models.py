ff91e51895faa54fb147b8c4772bc7f4bb2d735b
Alexander Balagura <alexanderbalagura@gmail.com>
2016-05-23 16:49:09 +0300

    Add Product model

--- product/models.py
+++ product/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(_('Name'), max_length=100)
+    slug = models.SlugField(_('Slug'), max_length=100)
+    price = models.DecimalField(_('Price'), decimal_places=2)
+    created_at = models.DateTimeField(_('Created time'), auto_now_add=True)
+    modified_at = models.DateTimeField(_('Modified time'), auto_now=True)
