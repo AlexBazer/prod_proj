30c31a4ed94c065e83f415ca1920699ff8647f71
Alexander Balagura <alexanderbalagura@gmail.com>
2016-05-27 14:23:05 +0300

    Add Product Item view with urls

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from product.views import Products
+from product.views import Products, ProductItem
 
 
 urlpatterns = [
-    url(r'products/', Products.as_view(), name='index')
+    url(r'^products/$', Products.as_view(), name='index'),
+    url(r'^products/(?P<slug>[-\w]+)$', ProductItem.as_view(), name='product_item')
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, ListView
+from django.views.generic import View, ListView, DetailView
 
 from product.models import Product
 
@@ -8,3 +8,8 @@ class Products(ListView):
     template_name = 'product/products.html'
     model = Product
     paginate_by = 10
+
+
+class ProductItem(DetailView):
+    template_name = 'product/product_item.html'
+    model = Product
