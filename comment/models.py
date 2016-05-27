8d0cfaefcd7b880b78e801e344254e896facc314
Alexander Balagura <alexanderbalagura@gmail.com>
2016-05-27 15:13:51 +0300

    Correct models Foreign key on_delete

--- comment/models.py
+++ comment/models.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import gettext as _
 
+from product.models import Product
 
 class Comment(models.Model):
     message = models.TextField(_('Message'))
     product = models.ForeignKey(
-        'product.Product',
+        Product,
         on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(_('Created time'), auto_now_add=True)
--- like/models.py
+++ like/models.py
@@ -7,8 +7,14 @@ from product.models import Product
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User)
-    product = models.ForeignKey(Product)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return 'use {username} likes "{product_name}"'.format(
