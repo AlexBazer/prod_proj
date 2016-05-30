485c47f2c141fcc5f7a5b35ffa8b364d751b7cd8
Alexander Balagura <alexanderbalagura@gmail.com>
2016-05-30 16:12:24 +0300

    Add create comment test

--- comment/tests.py
+++ comment/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from product.models import Product
+
+
+class ProductsTest(TestCase):
+    fixtures = ['products.json', ]
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_comment_add(self):
+        msg = 'Here is the message'
+        product = Product.objects.first()
+
+        response = self.client.post(reverse('product:product_item', kwargs={'slug': product.slug}), {
+            'message': msg,
+            'product': product.id,
+            'form_comment': ''
+        }, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('product/products.html')
+        self.assertIn(msg, response.content)
