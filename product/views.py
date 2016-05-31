e00f496ba5d0db264354e8a79047f9b2fd0b4f3c
Alexander Balagura <alexanderbalagura@gmail.com>
2016-05-31 11:28:41 +0300

    Add messages support and update forms in product view\

--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic import View, ListView, DetailView, TemplateView
+from django.utils.translation import gettext as _
 
 from product.models import Product
 from comment.models import Comment
@@ -7,6 +9,23 @@ from like.models import Like
 from comment.forms import CommentForm
 from like.forms import LikeForm
 
+from collections import namedtuple
+
+""" Form handler to be user with other forms in one view
+
+    attribures:
+    name: form name
+    form: form object
+    template_form: initiated form object
+    auth_pass: can be user to disable form processing
+    success_msg: message ufter form submission
+"""
+ViewForm = namedtuple('ViewForm', (
+    'name',
+    'form',
+    'auth_pass',
+    'success_msg'
+))
 
 class Products(ListView):
     template_name = 'product/products.html'
@@ -26,17 +45,26 @@ class ProductItem(DetailView):
 
         self.object = self.get_object()
         context = super(ProductItem, self).get_context_data(**kwargs)
-        context['form_comment'] = CommentForm(
-            initial={
-                'product': self.object.id
-            }
+
+        self.forms = {
+            'form_comment': ViewForm(
+                name='form_comment',
+                form=CommentForm,
+                auth_pass=True,
+                success_msg=_('Comment was successfully added')
+            ),
+            'form_like': ViewForm(
+                name='form_like',
+                form=LikeForm,
+                auth_pass=self.request.user.is_authenticated(),
+                success_msg=_("You've liked this product")
+            )
+        }
+        context['form_comment'] = self.forms['form_comment'].form(initial={'product': self.object.id})
+        context['form_like'] = self.forms['form_like'].form(initial={
+            'user': self.request.user.id, 'product': self.object.id}
         )
-        context['form_like'] = LikeForm(
-            initial={
-                'user': self.request.user.id,
-                'product': self.object.id
-            }
-        )
+
         context['comments'] = Comment.objects.filter(product=self.object)
 
         context['likes_count'] = Like.objects.filter(product=self.object).count()
@@ -53,20 +81,14 @@ class ProductItem(DetailView):
 
         context = self.get_context_data()
 
-        # Forms with template names than can be handled
-        # (<form name>, <formClass>, <login pass flag>)
-        forms = [
-            ('form_comment', CommentForm, True),
-            ('form_like', LikeForm, request.user.is_authenticated())
-        ]
-
         # go through forms and handle one in post was from it
-        for form_name, form, login_pass in forms:
-            if form_name not in request.POST or not login_pass:
+        for viewForm in self.forms.values():
+            if viewForm.name not in request.POST or not viewForm.auth_pass:
                 continue
-            context[form_name] = form(request.POST)
-            if context[form_name].is_valid():
-                context[form_name].save()
+            context[viewForm.name] = viewForm.form(request.POST)
+            if context[viewForm.name].is_valid():
+                context[viewForm.name].save()
+                messages.info(request, viewForm.success_msg)
                 return redirect('product:product_item', slug=self.object.slug)
             return render(request, self.template_name, context)
 
